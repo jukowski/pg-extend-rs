6c7a14a6e4f3af8ef92f061b1d1e784cad80e172
Constantin Jukowski <jucovschi@gmail.com>
2020-01-12 17:53:39 +0100

    add a test for longer strings

--- integration-tests/tests/strings.rs
+++ integration-tests/tests/strings.rs
@@ -31,3 +31,18 @@ fn test_text_rs() {
         assert_eq!(&col, "hello world!");
     });
 }
+
+#[test]
+fn test_long_text_rs() {
+    test_in_db("strings", |mut conn| {
+        let result = conn
+            .query("SELECT text_rs(array_to_string(ARRAY(SELECT chr(65) FROM generate_series(1,10000)), ''))", &[])
+            .expect("query failed");
+        assert_eq!(result.len(), 1);
+
+        let row = result.get(0).expect("no rows returned");
+        let col: String = row.get(0);
+
+        assert_eq!(*&col.len(), 10000);
+    });
+}
